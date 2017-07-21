6f3a2598449ee387d7a42ad575e51bd3bb90f7f8
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-07-21 17:14:18 +0700

    edit cdn path to dev-webcdn

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -6,9 +6,9 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'http://10.182.247.73/go-cdn-dev/node_modules/4.0.0/',
-      'go:': 'http://10.182.247.73/go-cdn-dev/dist/go/2.1.0/',
-      'lib:': 'http://10.182.247.73/go-cdn-dev/dist/lib/1.0.0/',
+      'npm:': 'http://dev-webcdn.cpall.co.th/node_modules/4.0.0/',
+      'go:': 'http://dev-webcdn.cpall.co.th/dist/go/2.1.0/',
+      'lib:': 'http://dev-webcdn.cpall.co.th/dist/lib/1.0.0/',
     },
     // map tells the System loader where to look for things
     map: {
